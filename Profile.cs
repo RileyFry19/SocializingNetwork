caf25f717e65f1be77809ea453dd7aa7af9ba9a8
LandonHarl <117372927+LandonHarl@users.noreply.github.com>
2024-05-14 16:07:09 -0500

    Add files via upload

---
+++ Profile.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheSocializingNetwork.Models;
+
+public class Profile
+{
+    [Key]
+    [Required]
+    public Guid ProfileId { get; set; }
+    [Required]
+    [RegularExpression(@"^\d+$", ErrorMessage = "UserId must be a valid number.")]
+    public Guid UserId { get; set; }
+    [Required]
+    public string? PictureUrl { get; set; }
+    
+    public User user { get; set; }
+}
